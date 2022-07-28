8293bc1ef4fac6baeb760de54fde3287725c3d58
hamzamahmood <hamzamehmood18@gmail.com>
2022-07-28 13:59:57 +0500

    duplicate events removed (by asim).

--- mipanel_python_application/main.py
+++ mipanel_python_application/main.py
@@ -2,6 +2,7 @@ import sys
 import json
 import events
 from datetime import date
+import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 from mixpanelexportapi.mixpanelexportapi_client import MixpanelexportapiClient
@@ -21,14 +22,6 @@ def get_events_from_mixpanel_api(from_date, to_date):
     project_id = 690335
     accept = 'text/plain'
     result = client.export_data.mixpanel_events_data(from_date, to_date, where, project_id, accept)
-
-    # # For Debugging purpose
-    # # open text file
-    # text_file = open("Events.txt", "w", encoding="utf8")
-    # # write string to file
-    # text_file.write(result)
-    # # close file
-    # text_file.close()
 
     return list(result.split("\n"))
 
@@ -50,6 +43,26 @@ def get_signup_email_list_from_events(json_list):
                 list.append(event['properties'].get('$username'))
     return list
 
+def get_counts(df):
+    counts_list = []
+    cgaas_events = df[((df.Event=='SignUp') & (df.SignUpProperty=='Anypoint')) |\
+       ((df.Event=='TransformViaAPI')) |\
+       ((df.Event=='Transform_Failed') & (df.EventProperty=='TransformViaAPI')) |\
+       ((df.Event=='Failed: SDKGenerated_API')) |\
+       ((df.Event=='SDKGenerated_API')) |\
+       ((df.Event=='OnPremPortalGeneration')) |\
+       ((df.Event=='OnPremPortalGenerationFailure'))].Email.nunique()
+    
+    commons_events = df[df.Event.str.contains("Transform")].Email.nunique()
+    codegen_events = df[df.Event.str.contains("SDKGenerated")].Email.nunique()
+    portal_events = df[df.Event.str.contains("Portal")].Email.nunique()
+    
+    counts_list.append(commons_events)
+    counts_list.append(codegen_events)
+    counts_list.append(portal_events)
+    counts_list.append(cgaas_events)
+    
+    return counts_list
 
 def get_filtered_event_count(week_number):
     commons_dict = {k: [] for k in events.CommonsEvents.list()}
@@ -67,9 +80,6 @@ def get_filtered_event_count(week_number):
 
     events_dict = {k: [] for k in events_list}
 
-    # For debugging purpose
-    # json_list = get_events_from_local_file()
-
     # -1 since our week is 1 week behind the iso calendar
     date_range = get_week_date_range(week_number - 1)
     print("Generating data for week {0}, from {1} to {2}.".format(week_number, date_range[0], date_range[1]))
@@ -77,7 +87,7 @@ def get_filtered_event_count(week_number):
 
     # get list of signup emails from the events
     signup_email_list = get_signup_email_list_from_events(json_list)
-
+    
     # list to filter duplicate email values
     event_email_list = []
 
@@ -86,54 +96,28 @@ def get_filtered_event_count(week_number):
             event = json.loads(json_str)
             event_name = event['event']
             user_email = event['properties'].get('$username')
+            event_property= event['properties'].get('InvokedFrom', 'NA')
+            signup_property= event['properties'].get('SignUpType', 'NA')
+            
+            
             # if event is relevant and is for new signup users
             if (event_name in events_dict.keys()) and (user_email in signup_email_list):
-                event_email = event_name + user_email
+                event_email = event_name +'-&-'+ user_email +'-&-'+ event_property +'-&-'+ signup_property
                 # to remove the duplicate events for same user
                 if event_email not in event_email_list:
                     events_dict[event_name].append(event)
                     event_email_list.append(event_email)
+    
+    
+    fin_lists = []
+    for row in event_email_list:
+        fin_lists.append(row.split('-&-'))
+        
+    df = pd.DataFrame(fin_lists, columns = ['Event', 'Email', 'EventProperty', 'SignUpProperty'])
+    df = df.drop_duplicates()
+    
+    return get_counts(df)
 
-    # populate commons event dictionary
-    for event in events.CommonsEvents.list():
-        commons_dict[event] = (events_dict[event])
-
-    # populate portal event dictionary
-    for event in events.PortalEvents.list():
-        portal_dict[event] = (events_dict[event])
-
-    # populate codeGen event dictionary
-    for event in events.CodeGenEvents.list():
-        codegen_dict[event] = (events_dict[event])
-
-    # populate cgaas event dictionary
-    for event in events.CGaaSEvents.list():
-        cgaas_dict[event] = (events_dict[event])
-
-    # process CGaaS signup events
-    signup_events = cgaas_dict[events.CGaaSEvents.SignUp.value]
-    filtered_signup_events = []
-    for se in signup_events:
-        value = se['properties'].get('SignUpType')
-        if value is not None and value == 'Anypoint':
-            filtered_signup_events.append(se)
-
-    # update filtered values
-    cgaas_dict[events.CGaaSEvents.SignUp.value] = filtered_signup_events
-
-    # process CGaaS transform_failed events
-    transform_failed_events = cgaas_dict[events.CGaaSEvents.Transform_Failed.value]
-    filtered_transform_failed_events = []
-    for tfe in transform_failed_events:
-        value = tfe['properties'].get('InvokedFrom')
-        if value is not None and value == 'TransformViaAPI':
-            filtered_transform_failed_events.append(value)
-
-    # update filtered values
-    cgaas_dict[events.CGaaSEvents.Transform_Failed.value] = filtered_transform_failed_events
-
-    return [get_dictionary_count(commons_dict), get_dictionary_count(portal_dict), get_dictionary_count(codegen_dict),
-            get_dictionary_count(cgaas_dict)]
 
 
 def get_week_date_range(week_number):
@@ -183,7 +167,7 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         raise TypeError("Invalid input. usage, app.py start_week_range end_week_range")
 
-    # process command line arguments
+    # process command line 
     start = int(sys.argv[1])
     end = int(sys.argv[2])
 
@@ -200,6 +184,7 @@ if __name__ == '__main__':
     input_range_list = get_week_list_from_range(start, end)
 
     for i in input_range_list:
+        
         events_per_week[i] = get_filtered_event_count(i)
 
     print(events_per_week)
